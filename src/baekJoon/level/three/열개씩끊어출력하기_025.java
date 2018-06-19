f3c3d73863af5b04aeb244bc45c7970ff9fa26e7
Yoon Young <yunyoung1819@hanmail.net>
2018-06-19 10:01:34 +0900

    피보나치 수열 풀이

---
+++ src/baekJoon/level/three/열개씩끊어출력하기_025.java
@@ -0,0 +1,25 @@
+package baekJoon.level.three;
+
+import java.util.Scanner;
+
+/**
+ * 문제번호 : 11721번
+ * 문제제목 : 열 개씩 끊어 출력하기
+ * 문제내용 : 알파벳 소문자와 대문자로만 이루어진 길이가 N인 단어가 주어진다.
+ * 한 줄에 10글자씩 끊어서 출력하는 프로그램을 작성하시오.
+ * 
+ * 입력 : 첫째 줄에 단어가 주어진다. 단어는 알파벳 소문자와 대문자로만 이루어져 있으며
+ * 길이는 100을 넘지 않는다. 길이가 0인 단어는 주어지지 않는다.
+ * 출력 : 입력으로 주어진 단어를 열 개씩 끊어서 한 줄에 하나씩 출력한다.
+ * 단어의 길이가 10의 배수가 아닌 경우에는 마지막 줄에는 10개 미만의 글자만 출력할 수도 있다.
+ * 
+ */
+public class 열개씩끊어출력하기_025 {
+	public static void main(String[] args) {
+		String s1 = "";
+		Scanner scan = new Scanner(System.in);
+		
+		s1 = scan.nextLine();
+		
+	}
+}
