247cb46dcefcd54267edd111a5edd1a04146bc01
Yoon Young <yunyoung1819@hanmail.net>
2018-08-21 11:20:12 +0900

    baekjoon 10950번 풀이 추가

--- src/baekJoon/level/four/APlusB03_039.java
+++ src/baekJoon/level/four/APlusB03_039.java
@@ -1,31 +1,27 @@
 package baekJoon.level.four;
 
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * 문제번호 : 10950번
- * 문제제목 : A + B - 3
+ * 문제제목 : A + B -3
  * 작 성 자 : Yun Young
  * 문제내용 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
- * 입    력 : 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
- * 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
- * 출    력 : 각 테스트 케이스마다 A+B를 출력한다. 
+ * 입    력 : 첫째 줄에 테스트 케이스의 개수 T가 주어진다. 
+ * 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0<A, B<10)
+ * 출    력 : 각 테스트 케이스마다 A+B를 출력한다.
  *
  */
 public class APlusB03_039 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		int[] a = new int[t];
-		int[] b = new int[t];
+		int t, a, b = 0;
+		t = sc.nextInt();
 		
-		for (int i = 0; i <= t; i++) {
-			a[i] = sc.nextInt();
-			b[i] = sc.nextInt();
-		}
-		
-		for(int j = 0; j <= t; j++) {
-			System.out.print(a[j]+b[j]);
+		while (t-- > 0) {
+			a = sc.nextInt();
+			b = sc.nextInt();
+			System.out.println(a + b);
 		}
 		sc.close();
 	}
