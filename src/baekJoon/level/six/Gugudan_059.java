5085228dbc73ad3958c6ae5b0b5d95fcc8d593b5
yunyoung1819 <yunyoung1819@hanmail.net>
2018-10-25 14:47:39 +0900

    baekjoon 2739번 문제 풀이 추가_2018.10.25_Ryan Yun

---
+++ src/baekJoon/level/six/Gugudan_059.java
@@ -0,0 +1,29 @@
+package baekJoon.level.six;
+
+import java.util.Scanner;
+
+/**
+ * 문제번호 :2739번
+ * 문제제목 : 구구단
+ * 작 성 자 : Ryan Yun
+ * 
+ * 문제내용 : N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오.
+ * 출력 형식에 맞춰서 출력하면 된다.
+ * 
+ * 입   력 : 첫째 줄에 N이 주어진다. N은 1보다 크거나 같고, 9보다 작거나 같다.
+ * 출   력 : 출력형식과 같게 N*1 부터 N*9 까지 출력한다.
+ */
+public class Gugudan_059 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i = n; i <= n; i++) {
+			for(int j = 1; j <=9; j++) {
+				System.out.println(i + " * " + j + " = " + (i*j));
+			}
+		}
+		
+		sc.close();
+	}
+}
