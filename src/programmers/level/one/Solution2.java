ab5d590418fee1470f088979d938f36cba551c2b
yunyoung1819 <yunyoung1819@hanmail.net>
2019-04-28 00:02:02 +0900

    programmers_같은숫자는싫어문제_풀이코드추가_2019.04.27_yunyoung

--- src/programmers/level/one/Solution2.java
+++ src/programmers/level/one/Solution2.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 /**
  * 문제 제목 : 같은 숫자는 싫어
+ * 문제 링크 : https://programmers.co.kr/learn/courses/30/lessons/12906
  * 
  * 문제 설명 : 배열 arr가 주어집니다. 
  * 배열 arr의 각 원소는 숫자 0부터 9까지로 이루어져 있습니다.
@@ -15,7 +16,7 @@ import java.util.ArrayList;
  * arr = [1,1,3,3,0,1,1]이면 [1,3,0,1]을 return 합니다.
  * arr = [4,4,4,3,3]이면 [4,3]을 return 합니다.
  * 
- * 배열 arr에서 엲속적으로 나타나는 숫자는 제거하고 남은 수들을 return 하는 solution 함수를 
+ * 배열 arr에서 속적으로 나타나는 숫자는 제거하고 남은 수들을 return 하는 solution 함수를 
  * 완성해주세요.
  *  
  * 제한 사항
@@ -23,7 +24,7 @@ import java.util.ArrayList;
  * 배열 arr의 원소의 크기 : 0보다 크거나 같고 9보다 작거나 같은 정수 
  */
 public class Solution2 {
-	public int[] solution(int []arr) {
+	public int[] solution(int[] arr) {
         int preNum = Integer.MAX_VALUE;
         ArrayList<Integer> tempList = new ArrayList<Integer>();
         
