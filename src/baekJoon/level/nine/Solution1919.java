ebd2c4524a9a32b63e883371e5ae1f86404cb0de
yunyoung1819 <yunyoung1819@hanmail.net>
2019-05-28 23:50:01 +0900

    백준 1919번 애너그램 만들기 문제 풀이코드 추가_2019.05.28_yunyoung

---
+++ src/baekJoon/level/nine/Solution1919.java
@@ -0,0 +1,37 @@
+package baekJoon.level.nine;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * @date 2019.05.24
+ * @author yun young
+ * 
+ * @문제번호 : 1919번 
+ * @문제제목 : 애너그램 만들기
+ * 
+ * @문제설명 : 두 영어 단어가 철자의 순서를 뒤바꾸어 같아질 수 있을 때, 그러한 두 단어를 서로 애너그램 관계에 있다고 한다.
+ * 예를 들면 occurs 라는 영어 단어와 succor는 서로 애너그램 관계에 있는데, occurs의 각 문자들의 순서를 잘 바꾸면
+ * succor이 되기 때문이다.
+ * 
+ * 한 편, dared와 bread는 서로 애너그램 관계에 있지 않다. 하지만 dared에서 맨 앞의 d를 제거하고, bread에서 제일 앞의 b를
+ * 제거하면, ared와 read라는 서로 애너그램 관계에 있는 단어가 남게 된다.
+ * 
+ * 두 개의 영어 단어가 주어졌을 때, 두 단어가 서로 애너그램 관계에 있도록 만들기 위해서 제거해야 하는 최소 개수의 문자 수를 구하는
+ * 프로그램을 작성하시오. 문자를 제거할 때에는 아무 위치에 있는 문자든지 제거할 수 있다.
+ * 
+ * @입력 : 첫째 줄과 둘째 줄에 영어 단어가 소문자로 주어진다. 각각의 길이는 1,000 자를 넘지 않으며, 적어도 한 글자로 이루어진 단어가 주어진다.
+ * @출력 : 첫째 줄에 답을 출력한다. 
+ *
+ *
+ */
+public class Solution1919 {
+	public static void main(String[] args) {
+		Solution1919 solution = new Solution1919();
+		solution.go();
+	}
+	
+	private void go() {
+		
+	}
+}
