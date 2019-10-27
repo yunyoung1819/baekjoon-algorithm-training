e0c3ff1fd3c9b864779fc30ff0597baa6b7f093a
yunyoung1819 <yunyoung1819@hanmail.net>
2019-10-28 06:01:39 +0900

    프로그래머스 sql 문제 풀이 추가

--- bin/programmers/sql/AnimalNameAndID.sql
+++ bin/programmers/sql/AnimalNameAndID.sql
@@ -3,6 +3,9 @@
  * 문제링크 : https://programmers.co.kr/learn/courses/30/lessons/59403
  * 작 성 일 : 2019.10.27
  * 작 성 자 : YUN YOUNG
+ * 
+ * 문제설명 : 동물 보호소에 들어온 모든 동물의 아이디와 이름을 ANIMAL_ID 순으로 조회하는
+ * SQL문을 작성해주세요. 
  */
 
 SELECT ANIMAL_ID, NAME
