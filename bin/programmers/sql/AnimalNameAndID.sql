c85727062e30ae4baf9c2e1559c2e21b69f0277c
yunyoung1819 <yunyoung1819@hanmail.net>
2019-10-27 11:15:34 +0900

    programmers sql 문제 풀이 추가_2019.10.27_yunyoung

---
+++ bin/programmers/sql/AnimalNameAndID.sql
@@ -0,0 +1,10 @@
+/**
+ * 문제제목 : 동물의 아이디와 이름
+ * 문제링크 : https://programmers.co.kr/learn/courses/30/lessons/59403
+ * 작 성 일 : 2019.10.27
+ * 작 성 자 : YUN YOUNG
+ */
+
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID ASC;
