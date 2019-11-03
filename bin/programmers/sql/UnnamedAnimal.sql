11063de2f17cc5fb7f42940bc3dbedf7d8cf9b06
yunyoung1819 <yunyoung1819@hanmail.net>
2019-11-03 12:37:39 +0900

    programmers sql 문제 풀이 추가_2019.11.03_yunyoung

---
+++ bin/programmers/sql/UnnamedAnimal.sql
@@ -0,0 +1,14 @@
+/**
+ * 문제제목: 이름이 없는 동물의 아이디
+ * 문제링크: https://programmers.co.kr/learn/courses/30/lessons/59039
+ * 작 성 일 : 2019.11.03
+ * 작 성 자 : YUN YOUNG
+ * DB: Mysql
+ * 
+ * 문제내용 : 동물 보호소에 들어온 동물 중, 이름이 없는 채로 들어온 동물의 ID를 조회하는 SQL 문을 작성해주세요.
+ * 단, ID는 오름차순 정렬되어야 합니다. 
+ **/
+SELECT ANIMAL_ID
+FROM ANIMAL_INS
+WHERE NAME IS NULL
+ORDER BY ANIMAL_ID ASC;
