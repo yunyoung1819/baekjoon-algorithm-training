89a1cb7f3f7ab684a62ce3554a434004dc0d8e9d
yunyoung1819 <yunyoung1819@hanmail.net>
2019-11-26 23:43:22 +0900

    programmers #문자열 다루기 기본 문제 풀이 추가_2019.11.26_yunyoung

---
+++ src/programmers/level/two/StringHandling.java
@@ -0,0 +1,25 @@
+package programmers.level.two;
+
+/**
+ * 문제번호 : Level 1.
+ * 문제제목 : 문자열 다루기 기본 
+ * 문제링크 : https://programmers.co.kr/learn/courses/30/lessons/12918
+ * 작 성 일 : 2019.11.26
+ * 작 성 자 : YUN YOUNG
+ * 문제내용 : 문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수 solution을 완성하세요. 
+ * 예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
+ *
+ */
+public class StringHandling {
+	public static void main(String[] args) {
+		StringHandling strHandling = new StringHandling();
+		strHandling.solution();
+	}
+
+	private void solution() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
