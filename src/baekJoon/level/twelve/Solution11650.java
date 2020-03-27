23106daa3c738675e1d87c3ddf22358e1aa2724e
yunyoung1819 <yunyoung1819@hanmail.net>
2020-03-27 23:15:10 +0900

    #백준 좌표 정렬하기 문제 풀이 추가_2020.03.27_yunyoung

---
+++ src/baekJoon/level/twelve/Solution11650.java
@@ -0,0 +1,16 @@
+package baekJoon.level.twelve;
+
+/**
+ * 문제번호 :
+ * 문제제목 : 
+ * 문제링크 :
+ * 작 성 일 : 
+ * 작 성 자 : 
+ * 문제내용 : 
+ *
+ */
+public class Solution11650 {
+	public static void main(String[] args) {
+		
+	}
+}
