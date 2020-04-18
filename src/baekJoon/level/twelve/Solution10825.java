b902003df96a2c1ecd6fba68237f378161a8734d
yunyoung1819 <yunyoung1819@hanmail.net>
2020-04-18 10:15:46 +0900

    baekjoon #10815 국영수 문제 풀이 추가_2020.04.18_yunyoung

---
+++ src/baekJoon/level/twelve/Solution10825.java
@@ -0,0 +1,14 @@
+package baekJoon.level.twelve;
+
+/**
+ * 문제번호 : 10825번 
+ * 문제제목 : 국영수 
+ * 문제링크 : https://www.acmicpc.net/problem/10825
+ * 작 성 일 : 
+ * 작 성 자 : 
+ * 문제내용 : 
+ *
+ */
+public class Solution10825 {
+
+}
