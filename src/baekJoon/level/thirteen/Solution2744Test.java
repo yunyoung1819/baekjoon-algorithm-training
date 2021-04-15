5c55331a88c5e2c12c90ba0da8d595ccfa7ac014
yunyoung1819 <yunyoung1819@hanmail.net>
2021-04-15 23:42:29 +0900

    baekjoon #2744번 대소문자 바꾸기 풀이코드 추가

---
+++ src/baekJoon/level/thirteen/Solution2744Test.java
@@ -0,0 +1,22 @@
+package baekJoon.level.thirteen;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+// Test Code
+public class Solution2744Test {
+
+    Solution2744 solution = new Solution2744();
+    String testWord1 = "WrongAnswer";
+    String testWord2 = "ILoveYou";
+
+    @Test
+    public void changeWordCase() {
+        assertEquals("wRONGaNSWER", solution.changeWordCase(testWord1));
+        assertEquals("ilOVEyOU", solution.changeWordCase(testWord2));
+    }
+
+}
