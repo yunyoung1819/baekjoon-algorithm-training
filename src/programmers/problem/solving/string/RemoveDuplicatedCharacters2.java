33b2c00bf2c5f0d19e450f66fa87c3bbf8618a3b
yunyoung1819 <yunyoung1819@hanmail.net>
2023-03-05 17:29:09 +0900

    :sparkles: [feature] Java 코딩 문제 이진트리 순회(깊이우선탐색) 풀이 코드 추가

--- src/programmers/problem/solving/string/RemoveDuplicatedCharacters2.java
+++ src/programmers/problem/solving/string/RemoveDuplicatedCharacters2.java
@@ -6,7 +6,7 @@ public class RemoveDuplicatedCharacters2 {
 	private void solution(String str) {
 		String answer = "";
 		for (int i=0; i < str.length(); i++) {
-//			System.out.println(str.charAt(i) + " " + i + " " + str.indexOf(str.charAt(i)));
+//			System.out.println'(str.charAt(i) + " " + i + " " + str.indexOf(str.charAt(i)));
 			if (str.indexOf(str.charAt(i)) == i) {
 				answer += str.charAt(i);
 			}
