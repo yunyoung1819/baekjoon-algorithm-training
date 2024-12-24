d8a1b913b13910208d68f1dfea3d097b71b8c401
yunyoung1819 <yunyoung1819@hanmail.net>
2024-12-24 10:42:44 +0900

    :sparkles: feat: 프로그래머스 문제 풀이

---
+++ src/programmers/challenge/level1/MemoryGame.java
@@ -0,0 +1,52 @@
+package programmers.challenge.level1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * title: 추억 점수
+ * date: 2024.12.24
+ * author: yun young
+ * link: https://school.programmers.co.kr/learn/courses/30/lessons/176963
+ * description: 사진들을 보며 추억에 젖어 있던 루는 사진별로 추억 점수를 매길려고 합니다.
+ * 사진 속에 나오는 인물의 그리움 점수를 모두 합산한 값이 해당 사진의 추억 점수가 됩니다.
+ * 예를 들어 사진 속 인물의 이름이 ["may", "kein", "kain"]이고 각 인물의 그리움 점수가 [5점, 10점, 1점]일 때
+ * 해당 사진의 추억 점수는 16(5 + 10 + 1)점이 됩니다.
+ *
+ * 다른 사진 속 인물의 이름이 ["kali", "mari", "don", "tony"]이고 ["kali", "mari", "don"]의 그리움 점수가
+ * 각각 [11점, 1점, 55점]]이고 "tony"는 그리움 점수가 없을 때,
+ * 이 사진의 추억 점수는 3명의 그리움 점수를 합한 67 (11 + 1 + 55)점 입니다.
+ * 그리워하는 사람의 이름을 담은 문자열 배열 name, 각 사람별 그리움 점수를 담은 정수 배열 yearning,
+ * 각 사진에 찍힌 인물의 이름을 담은 이차원 문자열 배열 photo가 주어질 때,
+ * 사진들의 추억 점수를 photo에 주어진 순서대로 배열에 담아 return 하는 solution 함수를 완성해주세요.
+ */
+public class MemoryGame {
+    public static void main(String[] args) {
+        MemoryGame memoryGame = new MemoryGame();
+        String[] name = {"may", "kein", "kain", "radi"};
+        int[] yearning = {5, 10, 1, 3};
+        String[][] photo = {{"may", "kein", "kain", "radi"}, {"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}};
+
+        int[] answer = memoryGame.solution(name, yearning, photo);
+
+        for (int a : answer) {
+            System.out.println(a);
+        }
+    }
+
+    private int[] solution(String[] name, int[] yearning, String[][] photo) {
+        Map<String, Integer> yearningMap = new HashMap<>();
+        int[] result = new int[photo.length];
+
+        for (int i = 0; i < name.length; i++) {
+            yearningMap.put(name[i], yearning[i]);
+        }
+
+        for (int i = 0; i < photo.length; i++) {
+            for (int j = 0; j < photo[i].length; j++) {
+                result[i] += yearningMap.getOrDefault(photo[i][j], 0);
+            }
+        }
+        return result;
+    }
+}
