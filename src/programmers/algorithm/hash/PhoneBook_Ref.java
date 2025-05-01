c54356b59db7f09230ab09c48ff7e2cc04b7d2e6
yunyoung1819 <yunyoung1819@hanmail.net>
2025-05-01 23:09:04 +0900

    :sparkles: feat: 프로그래머스 문제 풀이

--- src/programmers/algorithm/hash/PhoneBook_Ref.java
+++ src/programmers/algorithm/hash/PhoneBook_Ref.java
@@ -1,7 +1,30 @@
 package programmers.algorithm.hash;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class PhoneBook_Ref {
     public static void main(String[] args) {
+        String[] phoneBook = {"123", "456", "789"};
+        boolean result = solution(phoneBook);
+        System.out.println(result);
+    }
 
+    private static boolean solution(String[] phoneBook) {
+        Set<String> prefixes = new HashSet<>();
+
+        for (String phone : phoneBook) {
+            for (int end = 1; end < phone.length(); end++) {
+                prefixes.add(phone.substring(0, end));
+            }
+        }
+
+        for (String phone : phoneBook) {
+            if (prefixes.contains(phone)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
