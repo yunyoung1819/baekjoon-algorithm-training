dc138b44136d17f92e1f54aa39f612fbc8ec6a88
yunyoung1819 <yunyoung1819@hanmail.net>
2025-11-06 21:04:44 +0900

    :sparkles: feat: 프로그래머스 문제 풀이

---
+++ src/programmers/challenge/level1/K.java
@@ -0,0 +1,41 @@
+package programmers.challenge.level1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * title: K번째 수
+ * date: 2025.11.06
+ * author: yun young
+ * link: https://school.programmers.co.kr/learn/courses/30/lessons/42748
+ * description: 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
+ * 예를 들어 array가 [1,5,2,6,3,7,4], i = 2, j = 5, k = 3 이라면
+ * 1. array의 2번째부터 5번째까지 자르면 [5,2,6,3] 입니다.
+ * 2. 1에서 나온 배열을 정렬하면 [2,3,5,6] 입니다.
+ * 3. 2에서 나온 배열의 3번쨰 숫자는 5 입니다.
+ * 배열 array, [i,j,k]를 원소로 가진 2차원 배열 commands가 매개변수로 주어질 때, commands의 모든 원소에 대해 앞서 설명한
+ * 연산을 적용했을 때 나온 결과를 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+ *
+ */
+public class K {
+    public static void main(String[] args) {
+        int[] array = {1,5,2,6,3,7,4};
+        int[][] commands = {{2,5,3}, {4,4,1}, {1,7,3}};
+        int[] answer = solution(array, commands);
+        System.out.println(Arrays.toString(answer));
+    }
+
+    private static int[] solution(int[] array, int[][] commands) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < commands.length; i++) {
+            int from = commands[i][0], to = commands[i][1], idx = commands[i][2];
+            int[] subArray = Arrays.copyOfRange(array, from - 1, to);
+            Arrays.sort(subArray);
+            result.add(subArray[idx-1]);
+        }
+
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
