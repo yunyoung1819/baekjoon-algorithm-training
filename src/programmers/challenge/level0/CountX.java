57978da5751ddd4136e0a14fd4de68d3bdb6ea5e
yunyoung1819 <yunyoung1819@hanmail.net>
2025-11-30 14:03:29 +0900

    :sparkles: feat: 프로그래머스 문제 풀이

--- src/programmers/challenge/level0/CountX.java
+++ src/programmers/challenge/level0/CountX.java
@@ -1,29 +1,34 @@
 package programmers.challenge.level0;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * title: x 사이의 개수
- * date: 2025.01.12
+ * date: 2025.11.30
  * author: yun young
  * link: https://school.programmers.co.kr/learn/courses/30/lessons/181867
+ * description: 문자열 myString이 주어집니다. myString을 문자 "x"를 기준으로 나눴을 때
+ * 나눠진 문자열 각각의 길이를 순서대로 저장한 배열을 return하는 solution 함수를 완성해주세요.
  */
 public class CountX {
     public static void main(String[] args) {
-        String myString = "oxooxoxxox";
-        int[] result = solution(myString);
+        String myString1 = "oxooxoxxox";
+        System.out.println(Arrays.toString(solution(myString1)));
 
-        System.out.println(Arrays.toString(result));
+        String myString2 = "xabcxdefxghi";
+        System.out.println(Arrays.toString(solution(myString2)));
     }
 
     public static int[] solution(String myString) {
-        String[] parts = myString.split("x", -1); // split 메서드에 두번째 매개변수 -1을 추가하면 결과 배열 끝에 빈 문자열도 포함됨
-        int[] lengths = new int[parts.length];
+        List<Integer> lengths = new ArrayList<>();
+        String[] parts = myString.split("x", -1);
 
-        for (int i = 0; i < parts.length; i++) {
-            lengths[i] = parts[i].length();
+        for (String part : parts) {
+            lengths.add(part.length());
         }
 
-        return lengths;
+        return lengths.stream().mapToInt(Integer::intValue).toArray();
     }
 }
